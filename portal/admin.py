907b78405f64301919d502ab176b929bb4f97d3d
Lincoln Bryant <lincolnb@uchicago.edu>
2023-12-11 16:13:42 -0600

    Formatter

--- portal/admin.py
+++ portal/admin.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import base64
 from flask import session
 from dateutil.parser import parse
-from datetime import datetime 
+from datetime import datetime
 from datetime import timedelta
 from portal import logger
 from portal import app
@@ -13,19 +13,28 @@ from portal import app
 ciconnect_api_token = app.config["CONNECT_API_TOKEN"]
 ciconnect_api_endpoint = app.config["CONNECT_API_ENDPOINT"]
 mailgun_api_token = app.config["MAILGUN_API_TOKEN"]
-params = {'token': ciconnect_api_token}
+params = {"token": ciconnect_api_token}
+
 
 def authorized():
-    return session.get('admin') == 'admin'
+    return session.get("admin") == "admin"
+
 
 def get_usernames(group):
     if authorized():
         try:
-            url = ciconnect_api_endpoint + "/v1alpha1/groups/" + group + "/members?token=" + ciconnect_api_token
+            url = (
+                ciconnect_api_endpoint
+                + "/v1alpha1/groups/"
+                + group
+                + "/members?token="
+                + ciconnect_api_token
+            )
             users = requests.get(url).json()
-            return [member['user_name']for member in users['memberships']]
+            return [member["user_name"] for member in users["memberships"]]
         except:
-            logger.error('Error getting usernames')
+            logger.error("Error getting usernames")
+
 
 def get_user_profiles(group):
     if authorized():
@@ -34,88 +43,118 @@ def get_user_profiles(group):
 
             multiplex_json = {}
             for username in usernames:
-                multiplex_json['/v1alpha1/users/' + username + '?token=' + ciconnect_api_token] = {'method': 'GET'}
+                multiplex_json[
+                    "/v1alpha1/users/" + username + "?token=" + ciconnect_api_token
+                ] = {"method": "GET"}
 
-            url = ciconnect_api_endpoint + '/v1alpha1/multiplex'
+            url = ciconnect_api_endpoint + "/v1alpha1/multiplex"
             resp = requests.post(url, params=params, json=multiplex_json)
             data = resp.json()
 
             profiles = []
             for entry in data:
-                user = json.loads(data[entry]['body'])
-                username = user['metadata']['unix_name']
-                email = user['metadata']['email']
-                join_date = parse(user['metadata']['join_date']).strftime('%Y-%m-%d')
-                institution = user['metadata']['institution']
-                name = user['metadata']['name']
-                profiles.append({'username': username, 'email': email, 'join_date': join_date, 'institution': institution, 'name': name})
+                user = json.loads(data[entry]["body"])
+                username = user["metadata"]["unix_name"]
+                email = user["metadata"]["email"]
+                join_date = parse(user["metadata"]["join_date"]).strftime("%Y-%m-%d")
+                institution = user["metadata"]["institution"]
+                name = user["metadata"]["name"]
+                profiles.append(
+                    {
+                        "username": username,
+                        "email": email,
+                        "join_date": join_date,
+                        "institution": institution,
+                        "name": name,
+                    }
+                )
             return profiles
         except:
-            logger.error('Error getting user profiles')
+            logger.error("Error getting user profiles")
+
 
 def get_email_list(group):
     if authorized():
         email_list = []
         profiles = get_user_profiles(group)
         for profile in profiles:
-            email_list.append(profile['email'])
+            email_list.append(profile["email"])
         return email_list
+
 
 def update_user_institution(username, institution):
     if authorized():
         try:
-            json_data = {'apiVersion': 'v1alpha1', 'kind': 'User', 'metadata': {'institution': institution}}
+            json_data = {
+                "apiVersion": "v1alpha1",
+                "kind": "User",
+                "metadata": {"institution": institution},
+            }
             url = ciconnect_api_endpoint + "/v1alpha1/users/" + username
             resp = requests.put(url, params=params, json=json_data)
-            logger.info("Updated user %s. Set institution to %s." %(username, institution))
+            logger.info(
+                "Updated user %s. Set institution to %s." % (username, institution)
+            )
             return resp
         except Exception as err:
-            logger.error("Error updating institution for user %s." %username)
-    
+            logger.error("Error updating institution for user %s." % username)
+
+
 def email_users(sender, recipients, subject, body):
     if authorized():
-        try: 
+        try:
             logger.info("Sending email...")
-            resp = requests.post("https://api.mailgun.net/v3/api.ci-connect.net/messages",
+            resp = requests.post(
+                "https://api.mailgun.net/v3/api.ci-connect.net/messages",
                 auth=("api", mailgun_api_token),
                 data={
                     "from": "<" + sender + ">",
                     "to": [sender],
                     "bcc": recipients,
                     "subject": subject,
-                    "text": body
-                }
+                    "text": body,
+                },
             )
             return resp
         except:
             logger.error("Error sending email to all users")
 
+
 def plot_users_by_join_date(users):
     if authorized():
         try:
-            xvalues_format = "%m-%Y" 
+            xvalues_format = "%m-%Y"
             xvalues_set = set()
             for user in users:
-                join_date = datetime.strptime(user['join_date'], '%Y-%m-%d')
-                user['jd'] = join_date
-                xvalue = datetime(join_date.year, join_date.month, 1).strftime(xvalues_format)
+                join_date = datetime.strptime(user["join_date"], "%Y-%m-%d")
+                user["jd"] = join_date
+                xvalue = datetime(join_date.year, join_date.month, 1).strftime(
+                    xvalues_format
+                )
                 xvalues_set.add(xvalue)
             xvalues = list(xvalues_set)
-            xvalues.sort(key=lambda x:datetime.strptime(x, xvalues_format))
+            xvalues.sort(key=lambda x: datetime.strptime(x, xvalues_format))
             yvalues = [0] * len(xvalues)
             for i in range(len(xvalues)):
                 xvalue = datetime.strptime(xvalues[i], xvalues_format)
-                L = list(filter(lambda u : ((xvalue.year - u['jd'].year) * 12) + (xvalue.month - u['jd'].month) >= 0, users))
+                L = list(
+                    filter(
+                        lambda u: ((xvalue.year - u["jd"].year) * 12)
+                        + (xvalue.month - u["jd"].month)
+                        >= 0,
+                        users,
+                    )
+                )
                 yvalues[i] = len(L)
             fig = Figure(figsize=(16, 8), dpi=80, tight_layout=True)
             ax = fig.subplots()
             ax.plot(xvalues, yvalues)
-            ax.set_xlabel('Month')
-            ax.set_ylabel('Number of users')
-            ax.set_title('Number of users by month')
+            ax.set_xlabel("Month")
+            ax.set_ylabel("Number of users")
+            ax.set_title("Number of users by month")
             buf = BytesIO()
-            fig.savefig(buf, format='png')
+            fig.savefig(buf, format="png")
             data = base64.b64encode(buf.getbuffer()).decode("ascii")
             return data
         except:
-            logger.error('Error generating user by join date plot')
+            logger.error("Error generating user by join date plot")
